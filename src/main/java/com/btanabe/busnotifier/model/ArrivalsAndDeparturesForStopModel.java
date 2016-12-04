c1e5c9742dda9a931829c3f2420ee63104960489
Brian Tanabe <btanabe2@gmail.com>
2016-12-04 10:23:03 -0800

    Fixed a Lombok configuration bug.  I wasn't calling .equals() on superclass fields.  This revealed a test configuration issue as well.

--- src/main/java/com/btanabe/busnotifier/model/ArrivalsAndDeparturesForStopModel.java
+++ src/main/java/com/btanabe/busnotifier/model/ArrivalsAndDeparturesForStopModel.java
@@ -2,11 +2,15 @@ package com.btanabe.busnotifier.model;
 
 import com.btanabe.busnotifier.model.data.ArrivalsAndDeparturesForStopData;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 /**
  * Created by Brian on 11/26/16.
  */
 @Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true, includeFieldNames = true)
 public class ArrivalsAndDeparturesForStopModel extends Model {
     private ArrivalsAndDeparturesForStopData data;
 }
--- src/main/java/com/btanabe/busnotifier/model/StopsForLocation.java
+++ src/main/java/com/btanabe/busnotifier/model/StopsForLocation.java
@@ -2,11 +2,15 @@ package com.btanabe.busnotifier.model;
 
 import com.btanabe.busnotifier.model.data.StopsForLocationData;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 /**
  * Created by Brian on 12/1/16.
  */
 @Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true, includeFieldNames = true)
 public class StopsForLocation extends Model {
     private StopsForLocationData data;
 }
